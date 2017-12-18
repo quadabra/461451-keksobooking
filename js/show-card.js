59d07629b7c749ba90248b19c53954a6da5a5d2e
quadabra <gr.maks@gmail.com>
2017-12-18 23:49:26 +0300

    show-card.js

--- js/show-card.js
+++ js/show-card.js
@@ -1,20 +1,24 @@
 'use strict';
 
-window.showCard = function () {
-  var map = document.querySelector('.map');
-  var pins = map.querySelectorAll('.map__pin:not(.map__pin--main');
-  var cards = map.querySelectorAll('.map__card');
-  map.addEventListener('click', function (evt) {
-    if (!evt.target.classList.contains('map__pin')) {
-      return;
-    } else {
-      for (var i = 0; i < cards.length; i++) {
-        if (pins[i].classList.contains('map__pin--active')) {
-          cards[i].classList.remove('hidden');
-        } else if (!cards[i].classList.contains('hidden')) {
-          cards[i].classList.add('hidden');
+window = (function () {
+  return {
+    showCard: function () {
+      var map = document.querySelector('.map');
+      var pins = map.querySelectorAll('.map__pin:not(.map__pin--main');
+      var cards = map.querySelectorAll('.map__card');
+      map.addEventListener('click', function (evt) {
+        if (!evt.target.classList.contains('map__pin')) {
+          return;
+        } else {
+          for (var i = 0; i < cards.length; i++) {
+            if (pins[i].classList.contains('map__pin--active')) {
+              cards[i].classList.remove('hidden');
+            } else if (!cards[i].classList.contains('hidden')) {
+              cards[i].classList.add('hidden');
+            }
+          }
         }
-      }
+      });
     }
-  });
-};
+  }
+})();
