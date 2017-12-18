666eb1011b7aee76048af2393544019fc6964382
quadabra <gr.maks@gmail.com>
2017-12-18 23:56:21 +0300

    syncfields

--- js/synchronize-fields.js
+++ js/synchronize-fields.js
@@ -1,12 +1,6 @@
 'use strict';
 
-(function () {
-
-  window.synchronizeFields = function (inputElem, outElem, valuesIn, valuesOut, callback) {
-    var valueIndex = valuesIn.indexOf(inputElem.value);
-    callback(outElem, valuesOut[valueIndex]);
-  };
-
+window.synchronizeFields = (function () {
   var checkinTime = document.querySelector('#timein');
   var checkoutTime = document.querySelector('#timeout');
   var apartmentType = document.querySelector('#type');
@@ -16,15 +10,19 @@
   var apartmentTypes = ['bungalo', 'flat', 'house', 'palace'];
   var apartmentPrices = ['0', '1000', '5000', '10000'];
 
-  var syncValues = function(element, value) {
+  var syncValues = function (element, value) {
     element.value = value;
   };
 
-  var syncValueWithMin = function(element, value) {
+  var syncValueWithMin = function (element, value) {
     element.min = value;
   };
 
   window.synchronizeFields(checkinTime, checkoutTime, timeIn, timeOut, syncValues);
-  window.synchronizeFields(checkoutTime, checkoutTime, timeIn, timeOut, syncValues);
+  window.synchronizeFields(checkoutTime, checkinTime, timeOut, timeIn, syncValues);
   window.synchronizeFields(apartmentType, pricePerNight, apartmentTypes, apartmentPrices, syncValueWithMin);
+  return function (inputElem, outElem, valuesIn, valuesOut, callback) {
+      var valueIndex = valuesIn.indexOf(inputElem.value);
+      callback(outElem, valuesOut[valueIndex]);
+  };
 })();
