728c90188e4030187cee4d5ac978ab34c1c4b566
quadabra <gr.maks@gmail.com>
2017-12-20 13:49:21 +0300

    syncfields

--- js/backend.js
+++ js/backend.js
@@ -2,8 +2,27 @@
 
 window.backend = (function () {
   return {
-    load: function () {
+    load: function (onLoad, onError) {
+      var xhr = new XMLHttpRequest();
+      xhr.responseType = 'json';
 
+      xhr.addEventListener('load', function () {
+        if (xhr.status === 200) {
+          onLoad(xhr.response);
+        } else {
+          onError(xhr.response);
+        }
+        xhr.addEventListener('error', function () {
+          onError('Поломалося');
+        });
+        xhr.addEventListener('timeout', function () {
+          onError('Люди ау ' + xhr.timeout + 'мс');
+        });
+
+        xhr.timeout = 10000;
+
+        return xhr;
+      });
     },
     save: function () {
 
