d9cabc8b6ec5eb44386b61ad8677f10ee79c5d58
quadabra <gr.maks@gmail.com>
2017-12-20 14:12:55 +0300

    srv

--- js/backend.js
+++ js/backend.js
@@ -1,31 +1,42 @@
 'use strict';
 
 window.backend = (function () {
+  var SERVER_URL = 'https://1510.dump.academy/keksobooking/data';
+
+  var setup = function (onSuccess, onError) {
+    var xhr = new XMLHttpRequest();
+    xhr.responseType = 'json';
+
+    xhr.addEventListener('load', function () {
+      if (xhr.status === 200) {
+        onSuccess(xhr.response);
+      } else {
+        onError(xhr.response);
+      }
+    });
+    xhr.addEventListener('error', function () {
+      onError('Произошла ошибка соединения');
+    });
+    xhr.addEventListener('timeout', function () {
+      onError('Запрос не успел выполниться за ' + xhr.timeout + 'мс');
+    });
+
+    xhr.timeout = 10000;
+
+    return xhr;
+  };
   return {
     load: function (onLoad, onError) {
-      var xhr = new XMLHttpRequest();
-      xhr.responseType = 'json';
+      var xhr = setup(onLoad, onError);
 
-      xhr.addEventListener('load', function () {
-        if (xhr.status === 200) {
-          onLoad(xhr.response);
-        } else {
-          onError(xhr.response);
-        }
-        xhr.addEventListener('error', function () {
-          onError('Поломалося');
-        });
-        xhr.addEventListener('timeout', function () {
-          onError('Люди ау ' + xhr.timeout + 'мс');
-        });
-
-        xhr.timeout = 10000;
-
-        return xhr;
-      });
+      xhr.open('GET', SERVER_URL + '/data');
+      xhr.send();
     },
-    save: function () {
+    save: function (data, onSuccess, onError) {
+      var xhr = setup(onSuccess, onError);
 
+      xhr.open('POST', SERVER_URL);
+      xhr.send(data);
     }
   };
 })();
